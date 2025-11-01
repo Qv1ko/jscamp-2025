88530db50b3fa1ca1b16baa99aac337a331d74db
qv1ko <vicgarmur947@gmail.com>
2025-11-01 11:53:09 +0100

    feat: components fixed

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,13 +1,51 @@
 import { Header } from "./components/Header.jsx";
+import { JobListing } from "./components/JobListing.jsx";
+import { Pagination } from "./components/Pagination.jsx";
 import { SearchFormSection } from "./components/SearchFormSection.jsx";
+import { useState } from "react";
+
+import jobsData from "./assets/data.json";
+
+const RESULTS_PER_PAGE = 5;
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE);
+
+  const pageResults = jobsData.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE,
+    currentPage * RESULTS_PER_PAGE
+  );
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
       <Header />
 
       <main>
-        <SearchFormSection />
+        <section className="search-section">
+          <header>
+            <h1>Encuentra tu próximo trabajo</h1>
+            <p>Explora miles de oportunidades en el sector tecnológico.</p>
+          </header>
+
+          <SearchFormSection />
+
+          <section className="results-section">
+            <JobListing jobs={pageResults} />
+          </section>
+
+          <footer>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
+            />
+          </footer>
+        </section>
       </main>
     </>
   );
--- 02-react/src/components/SearchFormSection.jsx
+++ 02-react/src/components/SearchFormSection.jsx
@@ -1,12 +1,6 @@
 import { useState, useId } from "react";
 import { FilterSelector } from "./FilterSelector.jsx";
-import { JobListing } from "./JobListing.jsx";
-import { Pagination } from "./Pagination.jsx";
 import { SearchBar } from "./SearchBar.jsx";
-
-import jobsData from "../assets/data.json";
-
-const RESULTS_PER_PAGE = 5;
 
 export function SearchFormSection() {
   const idText = useId();
@@ -34,104 +28,73 @@ export function SearchFormSection() {
     // onTextFilter()
   };
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE);
-
-  const pageResults = jobsData.slice(
-    (currentPage - 1) * RESULTS_PER_PAGE,
-    currentPage * RESULTS_PER_PAGE
-  );
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
   return (
-    <section className="search-section">
-      <header>
-        <h1>Encuentra tu próximo trabajo</h1>
-        <p>Explora miles de oportunidades en el sector tecnológico.</p>
-      </header>
+    <section className="form-section">
+      <SearchBar
+        name={idText}
+        placeholder="Buscar trabajos, empresas o habilidades"
+      />
 
-      <section className="form-section">
-        <SearchBar
-          name={idText}
-          placeholder="Buscar trabajos, empresas o habilidades"
+      <form onSubmit={handleSubmit} id="filter" role="filter">
+        <FilterSelector
+          name={idTechnology}
+          options={[
+            { value: "", text: "Tecnología" },
+            { value: "azure", text: "Azure" },
+            { value: "aws", text: "AWS" },
+            { value: "cs", text: "C#" },
+            { value: "c++", text: "C++" },
+            { value: "firewalls", text: "Firewalls" },
+            { value: "gcp", text: "GCP" },
+            { value: "js", text: "JavaScript" },
+            { value: "kotlin", text: "Kotlin" },
+            { value: "linux", text: "Linux" },
+            { value: "node.js", text: "Node.js" },
+            { value: "nosql", text: "NoSQL" },
+            { value: "python", text: "Python" },
+            { value: "r", text: "R" },
+            { value: "react", text: "React" },
+            { value: "sql", text: "SQL" },
+            { value: "swift", text: "Swift" },
+            { value: "unity", text: "Unity" },
+            { value: "unreal", text: "Unreal Engine" },
+            { value: "vue.js", text: "Vue.js" },
+            { value: "windows", text: "Windows" },
+          ]}
         />
 
-        <form onSubmit={handleSubmit} id="filter" role="filter">
-          <FilterSelector
-            name={idTechnology}
-            options={[
-              { value: "", text: "Tecnología" },
-              { value: "azure", text: "Azure" },
-              { value: "aws", text: "AWS" },
-              { value: "cs", text: "C#" },
-              { value: "c++", text: "C++" },
-              { value: "firewalls", text: "Firewalls" },
-              { value: "gcp", text: "GCP" },
-              { value: "js", text: "JavaScript" },
-              { value: "kotlin", text: "Kotlin" },
-              { value: "linux", text: "Linux" },
-              { value: "node.js", text: "Node.js" },
-              { value: "nosql", text: "NoSQL" },
-              { value: "python", text: "Python" },
-              { value: "r", text: "R" },
-              { value: "react", text: "React" },
-              { value: "sql", text: "SQL" },
-              { value: "swift", text: "Swift" },
-              { value: "unity", text: "Unity" },
-              { value: "unreal", text: "Unreal Engine" },
-              { value: "vue.js", text: "Vue.js" },
-              { value: "windows", text: "Windows" },
-            ]}
-          />
-
-          <FilterSelector
-            name={idLocation}
-            options={[
-              { value: "", text: "Ubicación" },
-              { value: "mx", text: "Ciudad de México" },
-              { value: "mty", text: "Monterrey" },
-              { value: "hyb", text: "Híbrido" },
-              { value: "remote", text: "Remoto" },
-            ]}
-          />
-
-          <FilterSelector
-            name={idContactType}
-            options={[
-              { value: "", text: "Tipo de contrato" },
-              { value: "undefined", text: "Indefinido" },
-              { value: "replacement", text: "Sustitución" },
-              { value: "temporary", text: "Temporal" },
-            ]}
-          />
-
-          <FilterSelector
-            name={idExperience}
-            options={[
-              { value: "", text: "Nivel de experiencia" },
-              { value: "2", text: "2 años" },
-              { value: "3", text: "3 años" },
-              { value: "4", text: "4 años" },
-              { value: "5", text: "5 años" },
-            ]}
-          />
-        </form>
-      </section>
-
-      <section className="results-section">
-        <JobListing jobs={pageResults} />
-      </section>
-
-      <footer>
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
+        <FilterSelector
+          name={idLocation}
+          options={[
+            { value: "", text: "Ubicación" },
+            { value: "mx", text: "Ciudad de México" },
+            { value: "mty", text: "Monterrey" },
+            { value: "hyb", text: "Híbrido" },
+            { value: "remote", text: "Remoto" },
+          ]}
         />
-      </footer>
+
+        <FilterSelector
+          name={idContactType}
+          options={[
+            { value: "", text: "Tipo de contrato" },
+            { value: "undefined", text: "Indefinido" },
+            { value: "replacement", text: "Sustitución" },
+            { value: "temporary", text: "Temporal" },
+          ]}
+        />
+
+        <FilterSelector
+          name={idExperience}
+          options={[
+            { value: "", text: "Nivel de experiencia" },
+            { value: "2", text: "2 años" },
+            { value: "3", text: "3 años" },
+            { value: "4", text: "4 años" },
+            { value: "5", text: "5 años" },
+          ]}
+        />
+      </form>
     </section>
   );
 }
