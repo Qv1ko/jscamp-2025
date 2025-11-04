1e154a0a5b134db92985b4d667dc77b6378f3422
qv1ko <vicgarmur947@gmail.com>
2025-11-04 11:27:47 +0100

    feat: added focus and blur events

--- 02-react/src/components/SearchBar.jsx
+++ 02-react/src/components/SearchBar.jsx
@@ -1,6 +1,24 @@
+import styles from "./SearchBar.module.css";
+
+import { useState } from "react";
 import { Search } from "./icons/Search.jsx";
 
 export function SearchBar({ placeholder = "", handleTextSearch }) {
+  const [withError, setWithError] = useState(false);
+
+  const handleSearchFocus = (event) => {
+    const element = event.target;
+    element.classList.add(styles.writing);
+  };
+
+  const handleSearchValidation = (event) => {
+    const element = event.target;
+    const hasError = !/^([^0-9]*)$/.test(element.value);
+
+    setWithError(hasError);
+    element.classList.remove(styles.writing);
+  };
+
   return (
     <form id="search-form" role="search">
       <Search />
@@ -12,7 +30,11 @@ export function SearchBar({ placeholder = "", handleTextSearch }) {
         name="search"
         placeholder={placeholder}
         onChange={handleTextSearch}
+        onFocus={handleSearchFocus}
+        onBlur={handleSearchValidation}
       />
+
+      {withError && <span className={styles.error}>Error de validación</span>}
     </form>
   );
 }
